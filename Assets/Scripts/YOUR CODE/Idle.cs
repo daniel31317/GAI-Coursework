ba68783f082b087c283a6c611cd9b4d14a1b4b37
Daniel <crimpygymnast24@gmail.com>
2025-12-07 16:45:39 +0000

    added idle look out

--- Assets/Scripts/YOUR CODE/Idle.cs
+++ Assets/Scripts/YOUR CODE/Idle.cs
@@ -9,6 +9,19 @@ public class Idle : SteeringBehaviour
   
     public override Vector3 UpdateBehaviour(SteeringAgent steeringAgent)
     {
+
+        for (int j = 0; j < GameData.Instance.enemies.Count; j++)
+        {
+            if (!GameData.Instance.enemies[j].gameObject.activeSelf)
+            {
+                continue;
+            }
+            if (Algorithms.IsPositionInLineOfSight((Vector2)transform.position, (Vector2)GameData.Instance.enemies[j].transform.position))
+            {
+                AllyManager.Instance.FoundEnemyToAttack((EnemyAgent)GameData.Instance.enemies[j]);
+            }
+        }
+
         Vector3 targetOffset = transform.position - transform.position;
 
         desiredVelocity = targetOffset;
